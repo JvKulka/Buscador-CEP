a06e862cb329714ab2b76f7b6e7a1ba7e1594f07
João Victor Kulka <jv.kulka13@gmail.com>
2023-10-25 15:12:53 -0300

    Requisição API
    Realizado requisição para API e exibição para o usuário

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import "./style.css";
 
+import api from "./services/api";
+
 function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    if (input === "") {
+      alert("Preencha algum CEP!");
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Erro ao buscar");
+      setInput("");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscado de CEP</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu CEP..." />
+        <input
+          type="text"
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#fff" />
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: 83606598</h2>
-        <span>Rua Teste</span>
-        <span>Complemento: casa</span>
-        <span>Loteamento Helvídia</span>
-        <span>Campo Largo - PR</span>
-      </main>
+      {Object.keys(cep).length > 1 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   );
 }
