23c43bc1dbe95cbdda57abce19138f2664bc6b60
João Victor Kulka <jv.kulka13@gmail.com>
2023-10-25 11:20:05 -0300

    First Commit
    Iniciado desenvolvimento de projeto. Estrutura HTML realizada.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from "react-icons/fi";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscado de CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu CEP..." />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 83606598</h2>
+        <span>Rua Teste</span>
+        <span>Complemento: casa</span>
+        <span>Loteamento Helvídia</span>
+        <span>Campo Largo - PR</span>
+      </main>
     </div>
   );
 }
