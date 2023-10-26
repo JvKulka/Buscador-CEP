a7d4234dbf9dad13603d2e638ec7053389a2c840
João Victor Kulka <jv.kulka13@gmail.com>
2023-10-26 13:14:57 -0300

    Corrigido erro de ortográfia

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1 className="title">Buscado de CEP</h1>
+      <h1 className="title">Buscador de CEP</h1>
 
       <div className="containerInput">
         <input
